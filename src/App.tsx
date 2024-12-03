dfef8f4785eb7aeccd564b08c9736b85b74a4bb8
Cauê Garcia <cauerg7@outlook.com>
2024-12-03 10:05:01 -0300

    Links without full page reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
+import { Link } from "react-router-dom";
 
 export default function App() {
 
   return (
     <>
-      <h1>Bookkeeper!</h1>
+      <div>
+        <h1>Bookkeeper</h1>
+        <nav
+          style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
+          }}
+        >
+          <Link to="/invoices">Invoices</Link> |{" "}
+          <Link to="/expenses">Expenses</Link>
+        </nav>
+      </div>
     </>
   )
 }
